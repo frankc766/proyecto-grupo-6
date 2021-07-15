11a25a758f5d259d0b1bd190065a44dfba665ffd
frankc766 <85146275+frankc766@users.noreply.github.com>
2021-07-15 03:14:43 -0600

    grupo 6

---
+++ src/Helpers/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helpers;
+
+/**
+ *
+ * @author grupo 6 (Juan Cruz, Josue Gomez, Haniel Perez)
+ */
+public abstract class Persona {
+  
+}
---
+++ src/modulo6/trabajando_Github_grupo6.java
@@ -0,0 +1,24 @@
+/*
+ *Crear una superclase llamada Persona y 2 Subclases llamadas Doctor y Deportista. 
+(Cada integrante debe tener asignado ya sea la superclase o una de las dos subclases.)
+Crear los métodos necesarios para establecer y obtener su información personal aplicando los conceptos 
+estudiados y practicados hasta el momento. (herencia, abstracción, encapsulamiento y polimorfismo).
+ */
+package modulo6;
+
+
+/**
+ *
+ * @author grupo 6 (Juan Cruz, Josue Gomez, Haniel Perez)
+ */
+public class trabajando_Github_grupo6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+        
+        
+}
+}
